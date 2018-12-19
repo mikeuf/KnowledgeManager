3999de498f49a5f9b1ff28a1c30e3cedba989e73
Mike <mblack101@ufl.edu>
2018-12-18 22:56:21 -0500

    updated comments

--- src/bigws00client2/FXMLDocumentController.java
+++ src/bigws00client2/FXMLDocumentController.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bigws00client2;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,8 +13,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.web.HTMLEditor;
 
 /**
+ * FXMLDocumentController.fxml
  *
- * @author cyclops
+ * This is the controller for the application form. It presents a set
+ * of text fields into which the user can enter data to be saved to the
+ * database. Alternatively, the user can load existing articles from the
+ * database.
  */
 public class FXMLDocumentController implements Initializable {
 
@@ -45,6 +43,12 @@ public class FXMLDocumentController implements Initializable {
 	@FXML
 	private Label labelLoadStatus;
 
+	/**
+	 * When the user clicks Load, the entered article id is selected
+	 * from the database. The text fields are updated with the article data.
+	 * 
+	 * @param The button click event handler 
+	 */
 	@FXML
 	private void handleBtnLoadAction(ActionEvent event) {
 
@@ -71,6 +75,12 @@ public class FXMLDocumentController implements Initializable {
 		htmlFieldSolution.setHtmlText(solution);
 	}
 
+	/**
+	 * When the user clicks Save, the currently-entered content is saved to the
+	 * database for that article.
+	 * 
+	 * @param The button click event handler 
+	 */
 	@FXML
 	private void handleBtnSaveAction(ActionEvent event) {
 		labelLoadStatus.setVisible(false);
@@ -84,6 +94,12 @@ public class FXMLDocumentController implements Initializable {
 		saveArticle(list);
 	}
 
+		/**
+	 * When the user clicks New, the text fields are cleared and the Article ID
+	 * is set to the next available number. 
+	 * 
+	 * @param The button click event handler 
+	 */
 	@FXML
 	private void handleBtnNewAction(ActionEvent event) {
 		labelLoadStatus.setVisible(false);
@@ -97,36 +113,49 @@ public class FXMLDocumentController implements Initializable {
 
 	}
 
+	/**
+	 * Automatically sets the article ID to the next available number.
+	 * 
+	 * @param url
+	 * @param rb 
+	 */
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		int newArticleId = newArticle() + 1;
 		textFieldArticleId.setText(Integer.toString(newArticleId));
 	}
 
+	/**
+	 * Loads an article from the database
+	 *
+	 * @param The ID of the article to be loaded
+	 * @return The article, as a list of fields
+	 */
 	private static java.util.List<java.lang.String> loadArticle(int arg0) {
 		com.mycompany.knowledgemanager.DbInterface_Service service = new com.mycompany.knowledgemanager.DbInterface_Service();
 		com.mycompany.knowledgemanager.DbInterface port = service.getDbInterfacePort();
 		return port.loadArticle(arg0);
 	}
 
+	/**
+	 * Saves an article to the database
+	 *
+	 * @param The article as a list of fields
+	 */
 	private static void saveArticle(java.util.List<java.lang.String> arg0) {
 		com.mycompany.knowledgemanager.DbInterface_Service service = new com.mycompany.knowledgemanager.DbInterface_Service();
 		com.mycompany.knowledgemanager.DbInterface port = service.getDbInterfacePort();
 		port.saveArticle(arg0);
 	}
 
+	/**
+	 * Starts a new article and clears the fields
+	 *
+	 * @return The highest current article ID
+	 */
 	private static int newArticle() {
 		com.mycompany.knowledgemanager.DbInterface_Service service = new com.mycompany.knowledgemanager.DbInterface_Service();
 		com.mycompany.knowledgemanager.DbInterface port = service.getDbInterfacePort();
 		return port.newArticle();
 	}
-
-	private void exitApplication(ActionEvent event) {
-		Platform.exit();
-		System.exit(0);
-	}
-
-
-
-
 }
